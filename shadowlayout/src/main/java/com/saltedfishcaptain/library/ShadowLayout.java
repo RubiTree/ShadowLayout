f28323f6c27e997743aa7bcc7d888d68522c4bf6
bigbear <xiongzhengyu@hqyxjy.com>
2017-06-05 20:31:15 +0800

    add copyright

--- shadowlayout/src/main/java/com/saltedfishcaptain/library/ShadowLayout.java
+++ shadowlayout/src/main/java/com/saltedfishcaptain/library/ShadowLayout.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2017 SaltedFish Captain
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.saltedfishcaptain.library;
 
 import android.content.Context;
@@ -17,7 +33,7 @@ import android.widget.FrameLayout;
  * <p>
  * Attention:
  * <p>
- * Created by Zhengyu.Xiong ; On 2017-06-05.
+ * Created by SaltedFish Captain ; On 2017-06-05.
  */
 
 public class ShadowLayout extends FrameLayout {
@@ -31,7 +47,7 @@ public class ShadowLayout extends FrameLayout {
     private float shadowPaddingTop; // 阴影区域上方的缩进
     private float shadowPaddingBottom; // 阴影区域下方的缩进
 
-    private boolean invalidateShadowOnSizeChanged;
+    private boolean invalidateShadowOnSizeChanged; // size改变很快但改变得很小，对阴影的精确性要求不高但对性能要求较高时，可以把这个设置为false，并且手动调用invalidateShadow来刷新shadow
     private boolean forceInvalidateShadow = false;
 
     public ShadowLayout(Context context) {
@@ -100,18 +116,6 @@ public class ShadowLayout extends FrameLayout {
     /*--------------------------------------------------------------------------------------------*/
 
     @Override
-    protected int getSuggestedMinimumWidth() {
-        return 0;
-    }
-
-    @Override
-    protected int getSuggestedMinimumHeight() {
-        return 0;
-    }
-
-    /*--------------------------------------------------------------------------------------------*/
-
-    @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
         if (w > 0 && h > 0 && (getBackground() == null || invalidateShadowOnSizeChanged || forceInvalidateShadow)) {
@@ -171,6 +175,18 @@ public class ShadowLayout extends FrameLayout {
         canvas.drawRoundRect(shadowRect, cornerRadius, cornerRadius, shadowPaint);
 
         return output;
+    }
+
+    /*--------------------------------------------------------------------------------------------*/
+
+    @Override
+    protected int getSuggestedMinimumWidth() {
+        return 0;
+    }
+
+    @Override
+    protected int getSuggestedMinimumHeight() {
+        return 0;
     }
 
     /*--------------------------------------------------------------------------------------------*/
